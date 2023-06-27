66015403475127eb5bf760511105b0ec87e852d6
Dmitriy <pd2018@yandex.ru>
2023-06-27 20:13:14 +0300

    Added task_02

---
+++ task_02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class task_02 {
+    public static void main(String[] args) {
+        // Вычисление факториала.
+        Factorial();
+    }
+
+    /** Вычисление факториала числа. */
+    public static void Factorial() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Введите целое число: ");
+        int N = scanner.nextInt();
+        scanner.close();
+        
+        int sum = 1;
+        for (int i = 0; i < N; i++) {
+            sum *= i + 1;
+        }
+        System.out.println(sum);
+    }
+}
