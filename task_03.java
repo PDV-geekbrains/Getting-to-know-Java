c7742e4687769e1bbaa1111dab650a7ee5cb0222
Dmitriy <pd2018@yandex.ru>
2023-06-27 20:17:30 +0300

    Added task_03

---
+++ task_03.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class task_03 {
+    public static void main(String[] args) {
+        // Вывод простых чисел от 1 до 1000.
+        PrimeNumbers();
+    }
+
+        /** Вывод простых чисел от 1 до 1000. */
+    public static void PrimeNumbers() {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        
+        for (int i = 2; i <= 1000; i++) {
+            if (IsValid(i)) {
+                numbers.add(i);
+            }
+        }
+        
+        System.out.println(numbers);
+    }
+
+    static boolean IsValid(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
